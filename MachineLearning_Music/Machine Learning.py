9b740d133cc38deaea1b7d898bff055528ccce1f
zwolfe42 <85144283+zwolfe42@users.noreply.github.com>
2021-06-01 16:07:21 -0400

    Add files via upload

---
+++ MachineLearning_Music/Machine Learning.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import tree
+
+music_data = pd.read_csv('music.csv')
+X = music_data.drop(columns=['genre'])
+y = music_data['genre']
+
+model = DecisionTreeClassifier()
+model.fit(X, y)
+
+print(model.predict([[21, 0], [28,1]]))
+tree.plot_tree(model)
